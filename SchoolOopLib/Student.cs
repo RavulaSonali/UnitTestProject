5b513c2be3eecf43eeae9b8531d43400c4d362f7
Ravula Sonali <ravula.sonali@gmail.com>
2024-01-10 20:52:49 +0530

    UnitTestProject

---
+++ SchoolOopLib/Student.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolOopLib
+{
+    public class Student
+    {
+
+        private string _name;
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        private int _class;
+
+        public int Class
+        {
+            get { return _class; }
+            set { _class = value; }
+        }
+
+        private char _section;
+
+        public char Section
+        {
+            get { return _section; }
+            set { _section = value; }
+        }
+
+        //public Student(string n, int c, char s)
+        //{
+        //	Name = n;
+        //	Class = c;
+        //	Section = s;
+
+        //}
+
+        public bool Search(string s)
+        {
+            List<Student> students = new List<Student>();
+
+            students.Add(new Student { Name = "Deeraj", Class = 11, Section = 'D' });
+            students.Add(new Student { Name = "Shiva", Class = 10, Section = 'B' });
+            students.Add(new Student { Name = "Sathya", Class = 12, Section = 'C' });
+
+            var isfound = students.Find(d => d.Name == s);
+            return isfound != null;
+        }
+    }
+}
+
---
+++ UnitTestProject1/SchoolDataTests.cs
@@ -0,0 +1,57 @@
+﻿using NUnit.Framework;
+using SchoolOopLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestProject1
+{
+    [TestFixture]
+    public class SchoolDataTests
+    {
+        Student s = new Student();
+
+        Subject sub = new Subject();
+
+        Teacher ts = new Teacher();
+
+        [TestCase("Deeraj", ExpectedResult = true)]
+        [TestCase("Divya", ExpectedResult = false)]
+        [TestCase("Arjun", ExpectedResult = false)]
+        [TestCase("nanndini", ExpectedResult = false)]
+        [TestCase("Aakash", ExpectedResult = false)]
+        public bool IsName(string search)
+        {
+
+            return s.Search(search);
+
+        }
+
+        [TestCase("Maths", ExpectedResult = true)]
+        [TestCase("Science", ExpectedResult = true)]
+        [TestCase("Biology", ExpectedResult = false)]
+        [TestCase("English", ExpectedResult = true)]
+        public bool IsSubject(string subj)
+        {
+
+            return sub.Search(subj);
+
+        }
+
+        [TestCase("Oliver", ExpectedResult = true)]
+        [TestCase("Ramu", ExpectedResult = true)]
+        [TestCase("Ganesh", ExpectedResult = true)]
+        [TestCase("Murgan", ExpectedResult = false)]
+        public bool IsTeacher(string t)
+        {
+
+            return ts.Search(t);
+
+        }
+
+
+    }
+}
+
